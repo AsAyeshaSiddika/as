49db0a5df94bae8e16954342b2125ddf35df7934
Ayesha Siddika <73033546+AsAyeshaSiddika@users.noreply.github.com>
2020-12-06 08:55:07 +0600

    mid project commit

---
+++ mid/SellerHome.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Seller Home</title>
+</head>
+<body>
+	<h2>Welcome to Seller</h2>
+	<ul>
+				<li class="active">
+					<a href="SellerProfile.php">Profile</a>
+				</li>
+				<li class="Top">
+					<a href="topproduct.php">Top Product</a>
+				</li>
+				<li class="Prorang">
+					<a href="productrange.php">Product Range</a>
+				</li>
+				<li class="vieworder">
+					<a href="vieworder.php">View Order</a>
+				</li>
+				<li class="Buyer">
+					<a href="OurBuyer.php">Our Buyer</a>
+				</li>
+				<li class="Achievements">
+					<a href="Achievements.php">Achievements</a>
+				</li>
+				<li class="Partnership">
+					<a href="Partnership.php">Partnership</a>
+				</li>
+
+			</ul>
+
+</body>
+</html>
