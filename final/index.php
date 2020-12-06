1498d04a9fd9039e012e91747a76badbec9586f0
Ayesha Siddika <73033546+AsAyeshaSiddika@users.noreply.github.com>
2020-12-06 09:23:09 +0600

    final lab4 commit

---
+++ final/index.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body class="container">
+    <br><br>
+    <br><br>
+    <button type="button" onclick="document.location='../MVC/view/signUp.php'" class="btn btn-primary btn-lg">Sign Up</button>
+    <button type="button" onclick="document.location='../MVC/view/login.php'" class="btn btn-success btn-lg">Login</button>
+</body>
+</html>
