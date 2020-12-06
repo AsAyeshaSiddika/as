ac207338c3973fa608816cc1234f4817d0ef63f1
Ayesha Siddika <73033546+AsAyeshaSiddika@users.noreply.github.com>
2020-12-06 08:52:35 +0600

    mid project commit

---
+++ mid/SellerProfile.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<div style="display:inline-block;">
+    <?php include 'header.php' ?>
+  </div>
+	<title>Profile</title>
+</head>
+<body>
+	<h3>Seller Profile</h3>
+
+	<form action="sellerEdit.php" method="post">
+			<input type="submit" name="edit" value="Update Information" style="color:green; font-weigt:bold">
+			</form>
+
+			<form action="SellerHome.php" method="post">
+			<input type="submit" name="BuyerLogin" value="Back" style="color:green; font-weigt:bold">
+			</form>
+<form action="SellerLogin.php" method="post">
+			<input type="submit" name="BuyerLogin" value="Log Out" style="color:green; font-weigt:bold">
+			</form>
+<div>
+    <?php include 'footer.php' ?>
+  </div>
+</body>
+</html>
